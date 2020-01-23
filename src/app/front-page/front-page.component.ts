db73b373a93f4aece0c3a83ed2604450855b904c
Renny Mittal <rennymittal1234@gmail.com>
2020-01-23 21:00:26 +0530

    front page navigation

--- src/app/front-page/front-page.component.ts
+++ src/app/front-page/front-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from "@angular/router";
 
 @Component({
   selector: 'app-front-page',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrontPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {
+  
+   }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.router.navigate(['home']);
+  }, 3000);
   }
 
 }
