106dc1b72fbaaaf4d3efa4c9e9a9be3803b0f466
Renny Mittal <rennymittal1234@gmail.com>
2020-01-23 20:54:24 +0530

    front page done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: "start", component: FrontPageComponent},
+{path: "home", component: MainPageComponent},
+{path: "",redirectTo: "start",pathMatch: "full"}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    FrontPageComponent
   ],
   imports: [
     BrowserModule,
